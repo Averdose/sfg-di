f3c97d061aed229ad20c54c9a151ebffdc746bea
John Thompson <john@springframework.guru>
2019-12-27 19:57:34 +0100

    adding spring DI examples

--- src/main/java/aver/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/aver/springframework/sfgdi/SfgDiApplication.java
@@ -9,11 +9,25 @@ import org.springframework.context.ApplicationContext;
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
-		ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
-		MyController controller = (MyController) context.getBean("myController");
-		String greeting = controller.sayHello();
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
 		System.out.println(greeting);
+
+		System.out.println("------ Property");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("--------- Setter");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("-------- Constructor" );
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
 	}
 
 }
